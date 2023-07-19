cb720810ca22000e6bb52660e8bf95b5e5ac99dc
chaudhuree <chaudhuree00@gmail.com>
2023-07-20 04:13:06 +0600

    register ,login

--- libs/App.php
+++ libs/App.php
@@ -97,6 +97,35 @@ class App
 
     header("location:" . $path);
   }
+
+  // register user
+
+  public function register($query, $arr, $path)
+  {
+    if ($this->validate($arr) == "empty") {
+      echo "<script>alert('one or more fields are empty')</script>";
+    } else {
+      $register_user = $this->link->prepare($query);
+      $register_user->execute($arr);
+      // after inserting back to this given path
+      header("location:" . $path);
+    }
+  }
+
+  // login user
+
+  public function login($query, $data, $path)
+  {
+    $login_user = $this->link->query($query);
+    $login_user->execute();
+    $fetch = $login_user->fetch(PDO::FETCH_ASSOC);
+    if ($login_user->rowCount() > 0) {
+
+      if (password_verify($data['password'], $fetch['password'])) {
+        header("location:" . $path);
+      }
+    }
+  }
 }
 
 $app = new App();
